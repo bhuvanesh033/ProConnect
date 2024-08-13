6f5c866aed481d26193b62c0f6bbb714726743e3
GIRIDHARAN R <124025064+bhuvanesh033@users.noreply.github.com>
2024-08-13 18:59:57 +0530

    Update scripts and Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 80, // Use port from environment or default to 80
+  },
+});
