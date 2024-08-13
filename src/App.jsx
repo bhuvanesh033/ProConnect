9c64d99452de45aa9f5e1f03a3a5b7d1acecaf6a
GIRIDHARAN R <124025064+bhuvanesh033@users.noreply.github.com>
2024-08-13 20:36:32 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import store from './redux/store';
 import Homelayout from './components/Homelayout';
 import Job from './components/Jobs';
@@ -12,38 +12,45 @@ import Signup from './components/Signup';
 import CreateJob from './components/CreateJob';
 import MyJobs from './components/MyJobs';
 import ViewBids from './components/ViewBids';
-import SendMessage from './components/SendMessage';
+import SendMessage from './components/SendMessage'; // Adjust the path as needed
 import BidsList from './components/BidsList';
-import AcceptedBids from './components/AcceptedBids';  
+import AcceptedBids from './components/AcceptedBids';
 import Messages from './components/Messages';
 import Conversations from './components/Conversations';
 import ChatPage from './components/ChatPage';
-import ProtectedRoute from './components/ProtectedRoute';
-import './index.css';
+import './index.css'; // or wherever Tailwind CSS is imported
+
+const AppRoutes = () => {
+  const user = useSelector((state) => state.auth.user); // Get the user from Redux state
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={user ? <Homelayout /> : <Navigate to="/login" />}>
+        <Route path="/create-job" element={<CreateJob />} />
+        <Route path="/my-jobs" element={<MyJobs />} />
+        <Route path="/my-bids" element={<ViewBids />} />
+        <Route path="/" element={<Job />} />
+        <Route path="/applied-jobs" element={<AppliedJobs />} />
+        <Route path="/jobs/:jobId/bid" element={<BidForm />} />
+        <Route path="/send-message/:jobId" element={<SendMessage />} />
+        <Route path="/bids/:jobId" element={<BidsList />} />
+        <Route path="/accepted-bids" element={<AcceptedBids />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/conversations" element={<Conversations />} />
+        <Route path="/post-job" element={<PostJob />} />
+        <Route path="/chat/:conversationId" element={<ChatPage />} />
+      </Route>
+    </Routes>
+  );
+};
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<Homelayout />}>
-            <Route path="/" element={<ProtectedRoute element={<Job />} />} />
-            <Route path="/create-job" element={<ProtectedRoute element={<CreateJob />} />} />
-            <Route path="/my-jobs" element={<ProtectedRoute element={<MyJobs />} />} />
-            <Route path="/my-bids" element={<ProtectedRoute element={<ViewBids />} />} />
-            <Route path="/applied-jobs" element={<ProtectedRoute element={<AppliedJobs />} />} />
-            <Route path="/jobs/:jobId/bid" element={<ProtectedRoute element={<BidForm />} />} />
-            <Route path="/send-message/:jobId" element={<ProtectedRoute element={<SendMessage />} />} />
-            <Route path="/bids/:jobId" element={<ProtectedRoute element={<BidsList />} />} />
-            <Route path="/accepted-bids" element={<ProtectedRoute element={<AcceptedBids />} />} />
-            <Route path="/messages" element={<ProtectedRoute element={<Messages />} />} />
-            <Route path="/conversations" element={<ProtectedRoute element={<Conversations />} />} />
-            <Route path="/post-job" element={<ProtectedRoute element={<PostJob />} />} />
-            <Route path="/chat/:conversationId" element={<ProtectedRoute element={<ChatPage />} />} />
-          </Route>
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </Provider>
   );
